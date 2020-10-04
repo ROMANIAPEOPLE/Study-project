7cbea5ea4cdacf74c24229be3951681bc04411df
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-10-04 22:46:41 +0900

    유효성(중복/요구사항) 체크

--- src/main/java/com/study/studyolle/account/AccountController.java
+++ src/main/java/com/study/studyolle/account/AccountController.java
@@ -1,11 +1,29 @@
 package com.study.studyolle.account;
 
+import com.study.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    //signUpForm이라는 데이터를 받을때 함께 실행된다.
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
 
     @GetMapping("/sign-up")
     public String signUp(Model model){
@@ -13,4 +31,27 @@ public class AccountController {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword())  //암호화 해야함
+                .build();
+
+
+//        대체 가능
+//        signUpFormValidator.validate(signUpForm,errors);
+//        if(errors.hasErrors()) {
+//            return "account/sign-up";
+//        }
+
+        return "redirect:/";
+    }
+
 }
